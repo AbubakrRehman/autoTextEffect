ff0ce06a8cd56a65ca46163ff9dcf3b4533b2687
AbubakrRehman <abubakr.rehman91@outlook.com>
2023-07-12 14:07:15 +0530

    notes updated.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const text = document.getElementById("text");
+const speedElement = document.getElementById("speed");
+
+
+let speed = 300 / parseInt(speedElement.value);
 let data = text.dataset.text;
+text.innerHTML = "";
+let i = 0;
+let timer = null;
 
 
 
 
 function writeText() {
-    text.innerHTML = "";
-    let i = 0;
-    let timer = setInterval(() => {
+
+    timer = setInterval(function () {
         if (i < data.length) {
             console.log(data[i]);
             text.innerHTML += data[i];
@@ -18,8 +24,27 @@ function writeText() {
             // clearInterval(timer);
         }
 
-    }, 150)
+    }, speed)
 }
 
 writeText();
+
+
+speedElement.addEventListener("input", (e) => {
+    clearInterval(timer);
+    console.log("spedd");
+    speed = 300 / parseInt(e.target.value);
+    timer = setInterval(function () {
+        if (i < data.length) {
+            console.log(data[i]);
+            text.innerHTML += data[i];
+            i++;
+        } else {
+            i = 0;
+            text.innerHTML = "";
+            // clearInterval(timer);
+        }
+
+    }, speed)
+})
 
